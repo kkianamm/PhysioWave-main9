2cfc379ae1fa3f77c8ece9ef8ac20b5b9bf78042
kkianamm <kiana.taktehrani@gmail.com>
2026-01-23 12:46:05 -0500

    Update file path for h5 data source

--- std_checking.py
+++ std_checking.py
@@ -1,7 +1,8 @@
 import h5py, numpy as np
-p="/lambda/nfs/Kiana/PhysioWave/DB6_processed_8ch/train.h5"
+p="/lambda/nfs/Kiana/PhysioWave-main9/DB6_processed_8ch/train.h5"
 with h5py.File(p,"r") as f:
     x=f["data"][:1000]   # sample
 print("shape:", x.shape)
 print("min/max:", x.min(), x.max())
+
 print("mean/std:", x.mean(), x.std())
